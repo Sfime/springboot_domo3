b6ca6714e806e3869d78442d192bdde3cdf49975
HJK <15737178827@163.com>
2019-04-12 09:41:01 +0800

    update StudentController return map

--- src/main/java/com/baizhi/controller/StudentController.java
+++ src/main/java/com/baizhi/controller/StudentController.java
@@ -4,8 +4,11 @@ import com.baizhi.dao.StudentDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("student")
@@ -13,9 +16,12 @@ public class StudentController {
     @Autowired
     private StudentDao studentDao;
     @RequestMapping("query")
-    public String queryAll(){
+    public @ResponseBody
+    Map queryAll(){
+        Map map=new HashMap();
         List list = studentDao.selectAll();
+        map.put("list",list);
         System.out.println(list);
-        return "index";
+        return map;
     }
 }
